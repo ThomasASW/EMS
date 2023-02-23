a2a9e8aff422a621b0a66590115c7a4b54940194
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-02-23 11:12:46 +0530

    Completed profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,7 +6,8 @@ import { useEffect } from "react";
 import Card from "react-bootstrap/Card";
 
 const Profile = () => {
-  const [userDetails, setUserDetails] = useState();
+  const [userDetails, setUserDetails] = useState({});
+  const [roleMap, setRoleMap] = useState(new Map());
 
   useEffect(() => {
     axios
@@ -23,6 +24,23 @@ const Profile = () => {
           console.log("failed");
         }
       })
+      .then(() => {
+        axios
+          .get("http://localhost:3000/roles")
+          .then((res) => {
+            if (res.data) {
+              console.log(res.data);
+              var map = new Map();
+              res.data.forEach((element) => {
+                map.set(element.id, element.roleName);
+              });
+              setRoleMap(map);
+            } else {
+              console.log("failed");
+            }
+          })
+          .catch((error) => console.log(error));
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -34,14 +52,11 @@ const Profile = () => {
           <Card.Body>
             <Card.Title>{userDetails.name}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
-              Card Subtitle
+              {roleMap.get(userDetails.role)}
             </Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href={`mailto:${userDetails.email}`}>
+              {userDetails.email}
+            </Card.Link>
           </Card.Body>
         </Card>
       </Container>

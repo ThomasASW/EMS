8d7016dd3da730da80950c45899182bc1f4b975e
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-03-04 18:03:50 +0530

    Changed edit role to work directly from table

--- src/pages/EditRole.js
+++ src/pages/EditRole.js
@@ -1,103 +1,68 @@
 import axios from "axios";
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import NotifyModal from "../components/NotifyModal";
+import RoleForm from "../components/RoleForm";
 
 const EditRole = () => {
-  const [roleID, setRoleID] = useState("");
-  const [role, setRole] = useState("");
-  const [found, setFound] = useState(false);
-  const [modalHeader, setModalHeader] = useState("");
-  const [modalText, setModalText] = useState("");
-  const [showModal, setShowModal] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const handleRoleName = (event) => {
-    setRole(event.target.value);
+  const [initialValue, setInitialValue] = useState({});
+  const [notifyDetails, setNotifyDetails] = useState({});
+
+  const notify = (show, header, text) => {
+    setNotifyDetails({
+      showModal: show,
+      modalHeader: header,
+      modalText: text,
+    });
   };
 
-  const handleRoleID = (event) => {
-    setRoleID(event.target.value);
+  const close = () => {
+    if (notifyDetails.modalHeader === "Success") {
+      navigate(-1);
+    } else {
+      setNotifyDetails({
+        showModal: false,
+        modalHeader: "",
+        modalText: "",
+      });
+    }
   };
+
+  useEffect(() => {
+    axios(`http://localhost:3000/roles/${id}`)
+      .then((response) => {
+        setInitialValue(response.data.roleName);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        notify(true, "Error", "ID does not exist");
+      });
+  }, [id]);
 
   const handleSuccess = () => {
-    setModalHeader("Success");
-    setModalText("Role updated successfully");
-    setShowModal(true);
-    setTimeout(() => {
-      setRoleID("");
-      setRole("");
-      setShowModal(false);
-    }, 2500);
+    notify(true, "Success", "Role updated successfully");
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event, role) => {
     event.preventDefault();
-    if (found) {
-      await axios
-        .put(`http://localhost:3000/roles/${roleID}`, {
-          id: roleID,
-          roleName: role,
-        })
-        .then(handleSuccess)
-        .catch((error) => console.log(error));
-    } else {
-      await axios(`http://localhost:3000/roles/${roleID}`)
-        .then((response) => {
-          setRole(response.data.roleName);
-          setFound(true);
-        })
-        .catch((error) => {
-          console.log(error.message);
-          setModalHeader("Error");
-          setModalText("ID does not exist");
-          setShowModal(true);
-          setTimeout(() => {
-            setShowModal(false);
-          }, 2500);
-        });
-    }
+    await axios
+      .put(`http://localhost:3000/roles/${id}`, {
+        id: id,
+        roleName: role,
+      })
+      .then(handleSuccess)
+      .catch((error) => console.log(error));
   };
 
   return (
     <>
-      <Modal size="sm" show={showModal} backdrop="static" keyboard={false}>
-        <Modal.Header>
-          <Modal.Title id="example-modal-sizes-title-sm">
-            {modalHeader}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{modalText}</Modal.Body>
-      </Modal>
+      <NotifyModal modalDetails={notifyDetails} handleInput={close} />
       <div className="restGrid">
         <h2>Edit role</h2>
-        <Form onSubmit={(event) => handleSubmit(event)}>
-          <Form.Group className="mb-3" controlId="formBasicID">
-            <Form.Label>Role ID</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter role ID"
-              value={roleID}
-              onChange={(event) => handleRoleID(event)}
-            />
-          </Form.Group>
-          {found ? (
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Role Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter role name"
-                value={role}
-                onChange={(event) => handleRoleName(event)}
-              />
-            </Form.Group>
-          ) : (
-            <></>
-          )}
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+        <RoleForm onSubmit={handleSubmit} initialValue={initialValue} />
       </div>
     </>
   );

4baafc21445af25bc310d71a9cff0ec8f7e52127
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-03-09 19:59:30 +0530

    Updated table tests

--- src/components/BootstrapTable.test.js
+++ src/components/BootstrapTable.test.js
@@ -7,101 +7,65 @@ const data = [
   {
     id: 1,
     roleName: "Admin",
+    editFn: jest.fn(() => console.log(1)),
+    deleteFn: jest.fn(() => console.log(1)),
   },
   {
     id: 2,
     roleName: "Employee",
+    editFn: jest.fn(() => console.log(2)),
+    deleteFn: jest.fn(() => console.log(2)),
   },
   {
     id: 3,
     roleName: "Consultant",
+    editFn: jest.fn(() => console.log(3)),
+    deleteFn: jest.fn(() => console.log(3)),
   },
   {
-    roleName: "Manager",
     id: 4,
+    roleName: "Manager",
+    editFn: jest.fn(() => console.log(4)),
+    deleteFn: jest.fn(() => console.log(4)),
   },
 ];
 
 const headers = ["#", "Name", "Operations"];
 
 test("Bootstrap Table Data Render Test", async () => {
-  const editFn = jest.fn((id) => {
-    console.log(id);
-  });
-  const deleteFn = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <BootstrapTable
-      headers={headers}
-      data={data}
-      editFn={editFn}
-      deleteFn={deleteFn}
-    />
-  );
+  render(<BootstrapTable headers={headers} data={data} />);
   const roleName = screen.getByText("Admin");
   expect(roleName).toBeInTheDocument();
 });
 
 test("Bootstrap Table Header Render Test", async () => {
-  const editFn = jest.fn((id) => {
-    console.log(id);
-  });
-  const deleteFn = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <BootstrapTable
-      headers={headers}
-      data={data}
-      editFn={editFn}
-      deleteFn={deleteFn}
-    />
-  );
+  render(<BootstrapTable headers={headers} data={data} />);
   const headerName = screen.getByText("Operations");
   expect(headerName).toBeInTheDocument();
 });
 
 test("Bootstrap Table Edit Test", async () => {
-  const editFn = jest.fn((id) => {
-    console.log(id);
+  const editFn = jest.fn(() => {
+    console.log("editFn");
   });
-  const deleteFn = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <BootstrapTable
-      headers={headers}
-      data={data}
-      editFn={editFn}
-      deleteFn={deleteFn}
-    />
-  );
+  let newData = [...data];
+  newData[0].editFn = editFn;
+  render(<BootstrapTable headers={headers} data={newData} />);
   const editButton = screen.getAllByAltText("Edit Button")[0];
   userEvent.click(editButton);
   expect(editFn).toBeCalled();
   expect(editFn).toBeCalledTimes(1);
-  expect(editFn).toBeCalledWith(1);
 });
 
 test("Bootstrap Table Delete Test", async () => {
-  const editFn = jest.fn((id) => {
-    console.log(id);
+  const deleteFn = jest.fn(() => {
+    console.log("deleteFn");
   });
-  const deleteFn = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <BootstrapTable
-      headers={headers}
-      data={data}
-      editFn={editFn}
-      deleteFn={deleteFn}
-    />
-  );
+  let newData = [...data];
+  newData[0].deleteFn = deleteFn;
+  render(<BootstrapTable headers={headers} data={newData} />);
   const deleteButton = screen.getAllByAltText("Delete Button")[0];
   userEvent.click(deleteButton);
   expect(deleteFn).toBeCalled();
   expect(deleteFn).toBeCalledTimes(1);
-  expect(deleteFn).toBeCalledWith(1);
 });

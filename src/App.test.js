741223e1011c4010a7086fb38adbe96390ba22d9
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-03-09 21:15:45 +0530

    Updated app tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,21 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import App from "./App";
+const mockSelector = jest.fn(() => {
+  return {
+    showModal: false,
+    modalHeader: "",
+    modalText: "",
+    isConfirm: false,
+    closeCallback: undefined,
+    confirmCallback: undefined,
+  };
+});
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useSelector: () => mockSelector,
+  useDispatch: () => mockDispatch,
+}));
 
 test("App - login page render test", () => {
   render(
@@ -14,6 +29,8 @@ test("App - login page render test", () => {
 });
 
 test("App render test", () => {
+  jest.spyOn(Storage.prototype, "getItem");
+  Storage.prototype.getItem = jest.fn().mockReturnValue("1");
   render(
     <MemoryRouter initialEntries={[{ pathname: "/profile" }]}>
       <App />

0b6d35304f07cc139a4627d01fc7ea911096e101
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-03-09 20:39:20 +0530

    Updated add employee page tests

--- src/pages/AddEmployee.test.js
+++ src/pages/AddEmployee.test.js
@@ -2,6 +2,10 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AddEmployee from "./AddEmployee";
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: () => mockDispatch,
+}));
 
 test("AddEmployee Render Test", () => {
   render(

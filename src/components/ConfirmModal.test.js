65d31fe4ccbee226e0e6e8d81305893efd57a142
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-03-09 20:18:52 +0530

    Fix confirm dialog test

--- src/components/ConfirmModal.test.js
+++ src/components/ConfirmModal.test.js
@@ -2,20 +2,21 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import ConfirmModal from "./ConfirmModal";
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: () => mockDispatch,
+}));
 
 test("Confirm Modal Visible Test", async () => {
   const modalDetails = {
     showModal: true,
+    isConfirm: true,
     modalHeader: "Success",
     modalText: "Successful",
-    id: 1,
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <ConfirmModal modalDetails={modalDetails} handleInput={handleInput} />
-  );
+  render(<ConfirmModal modalDetails={modalDetails} />);
   const header = screen.getByText("Success");
   expect(header).toBeInTheDocument();
 });
@@ -23,16 +24,13 @@ test("Confirm Modal Visible Test", async () => {
 test("Confirm Modal Invisible Test", async () => {
   const modalDetails = {
     showModal: false,
+    isConfirm: true,
     modalHeader: "Success",
     modalText: "Successful",
-    id: 1,
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <ConfirmModal modalDetails={modalDetails} handleInput={handleInput} />
-  );
+  render(<ConfirmModal modalDetails={modalDetails} />);
   const header = screen.queryByText("Success");
   expect(header).not.toBeInTheDocument();
 });
@@ -40,39 +38,31 @@ test("Confirm Modal Invisible Test", async () => {
 test("Confirm Modal Input Cancel Test", async () => {
   const modalDetails = {
     showModal: true,
+    isConfirm: true,
     modalHeader: "Success",
     modalText: "Successful",
-    id: 1,
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <ConfirmModal modalDetails={modalDetails} handleInput={handleInput} />
-  );
+  render(<ConfirmModal modalDetails={modalDetails} />);
   const cancelButton = screen.getByText("Cancel");
   userEvent.click(cancelButton);
-  expect(handleInput).toBeCalled();
-  expect(handleInput).toBeCalledTimes(1);
-  expect(handleInput).toBeCalledWith(-1);
+  expect(mockDispatch).toBeCalled();
+  expect(mockDispatch).toBeCalledTimes(1);
 });
 
 test("Confirm Modal Input Confirm Test", async () => {
   const modalDetails = {
     showModal: true,
+    isConfirm: true,
     modalHeader: "Success",
     modalText: "Successful",
-    id: 1,
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn((id) => {
-    console.log(id);
-  });
-  render(
-    <ConfirmModal modalDetails={modalDetails} handleInput={handleInput} />
-  );
+  render(<ConfirmModal modalDetails={modalDetails} />);
   const cancelButton = screen.getByText("Confirm");
   userEvent.click(cancelButton);
-  expect(handleInput).toBeCalled();
-  expect(handleInput).toBeCalledTimes(1);
-  expect(handleInput).toBeCalledWith(1);
+  expect(mockDispatch).toBeCalled();
+  expect(mockDispatch).toBeCalledTimes(1);
 });

c21880bce400f1edebfef8b1acf119fa0bfa5f6a
Thomas <125454436+ThomasASW@users.noreply.github.com>
2023-03-09 20:36:37 +0530

    Updated notify modal tests

--- src/components/NotifyModal.test.js
+++ src/components/NotifyModal.test.js
@@ -2,17 +2,21 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import NotifyModal from "./NotifyModal";
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: () => mockDispatch,
+}));
 
 test("Notify Modal Visible Test", async () => {
   const modalDetails = {
     showModal: true,
+    isConfirm: false,
     modalHeader: "Success",
     modalText: "Successful",
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn(() => {
-    console.log("close");
-  });
-  render(<NotifyModal modalDetails={modalDetails} handleInput={handleInput} />);
+  render(<NotifyModal modalDetails={modalDetails} />);
   const header = screen.getByText("Success");
   expect(header).toBeInTheDocument();
 });
@@ -20,13 +24,13 @@ test("Notify Modal Visible Test", async () => {
 test("Notify Modal Invisible Test", async () => {
   const modalDetails = {
     showModal: false,
+    isConfirm: false,
     modalHeader: "Success",
     modalText: "Successful",
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn(() => {
-    console.log("close");
-  });
-  render(<NotifyModal modalDetails={modalDetails} handleInput={handleInput} />);
+  render(<NotifyModal modalDetails={modalDetails} />);
   const header = screen.queryByText("Success");
   expect(header).not.toBeInTheDocument();
 });
@@ -34,15 +38,15 @@ test("Notify Modal Invisible Test", async () => {
 test("Notify Modal Input Close Test", async () => {
   const modalDetails = {
     showModal: true,
+    isConfirm: false,
     modalHeader: "Success",
     modalText: "Successful",
+    closeCallback: undefined,
+    confirmCallback: undefined,
   };
-  const handleInput = jest.fn(() => {
-    console.log("close");
-  });
-  render(<NotifyModal modalDetails={modalDetails} handleInput={handleInput} />);
+  render(<NotifyModal modalDetails={modalDetails} />);
   const cancelButton = screen.getByText("Close");
   userEvent.click(cancelButton);
-  expect(handleInput).toBeCalled();
-  expect(handleInput).toBeCalledTimes(1);
+  expect(mockDispatch).toBeCalled();
+  expect(mockDispatch).toBeCalledTimes(1);
 });
